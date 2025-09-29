d30c31354b418e99e20ddd3c0fbc7f1f4b6fa0c2
Katisvektavir <86384829+Katisvektavir@users.noreply.github.com>
2025-09-30 00:06:30 +0300

    Add files via upload

--- test.cpp
+++ test.cpp
@@ -6,6 +6,8 @@
 #include<cctype>
 #include<queue>
 #include<stack>
+#include<fstream>
+#include <typeinfo>
 
 using namespace std;
 using namespace nlohmann;
@@ -14,11 +16,23 @@ using namespace nlohmann;
 map<string, vector<int>> operations = {{"not", {10, 1}}, {"all", {10, -1}}, {"exist", {10, -1}},{"->", {3, 2}}};
 
 
-void shuting_yard(string &token, queue<string> & polish, stack<string> &op){
+template<typename T>
+void modus_ponens(T& form, vector<string> antecedent){
+  form["conclusion"].push_back({{{"formul", antecedent}, {"conclusion", vector<string>{}}}});
+  cout << form["formul"];
+  for(string i : form["formul"]){
+      antecedent.push_back(i);
+    }
+  antecedent.push_back("->");
+  form["conclusion"].push_back({{{"formul", antecedent}, {"conclusion", vector<string>{}}}});
+  
+}
+
+void shuting_yard(string &token, vector<string> & polish, stack<string> &op){
   if(!op.empty()){
     if(operations[token][0] <= operations[op.top()][0]){
       while(!op.empty()){
-	polish.push(op.top());
+	polish.push_back(op.top());
 	op.pop();
       }
     }
@@ -31,12 +45,14 @@ vector<vector<string>> synonyms = {};
 
 
 int main(){
-  queue<string> polish;
+  vector<string> polish;
   stack<string> op;
-  json data;
   string input = "";
   string token= "";
   bool alnum = false;
+  json sequence;
+
+
   getline(cin, input);
   for(char i : input){
     
@@ -58,7 +74,7 @@ int main(){
 	  shuting_yard(token, polish, op);
 	}
 	else{
-	  polish.push(token);
+	  polish.push_back(token);
 	  token = "";
 	}
       }
@@ -74,20 +90,33 @@ int main(){
 	shuting_yard(token, polish, op);
       }
       else if(alnum){
-	polish.push(token);
+	polish.push_back(token);
       }
       else{
 	cout << "invalid sintax, operator " + token + " dosn`t exist" + "\n";
       }
     }
-  while(not op.empty()){
-    polish.push(op.top());
+
+   while(not op.empty()){
+    polish.push_back(op.top());
     op.pop();
   }
-  while(not polish.empty()){
-    cout << polish.front() << "\n";
-    polish.pop();
+
+  for(string i : polish){
+    cout << i << " ";
   }
+  cout << "\n";
+  sequence["formul"] = vector<string>{polish};
+  sequence["conclusion"] = {};
+
   
-  return 0;}
+  json * leaf = &(sequence["conclusion"]);
+  vector<json *> leafs = {leaf};
+  modus_ponens(sequence, vector<string>{"3", "->", "lol"});
   
+  cout << sequence.dump(4) << "\n";
+  ofstream file("./seq_json/seq_tree2");
+  
+  file << sequence.dump(4);
+  return 0;
+}  
